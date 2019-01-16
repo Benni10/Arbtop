b995ac8dc6787f6f55d92551b02b115d1b720c65
Benni10 <46264219+Benni10@users.noreply.github.com>
2019-01-16 20:03:06 +0100

    Add files via upload

--- startbootstrap-business-casual-gh-pages/Warenkorb/warenkorb.php
+++ startbootstrap-business-casual-gh-pages/Warenkorb/warenkorb.php
@@ -39,6 +39,58 @@ require ('header_war.php');
         </div>
     </div>
 </nav>
+<script>
+    'use strict';
+    function warnung(e) {
+        var check = confirm('Wollen Sie diesen Eintrag wirklich löschen?');
+        if (check == false) {
+            mySubmitFunction(e);
+        }
+    }
+
+    function mySubmitFunction(e) {
+        e.preventDefault();
+        return false;
+    }
+</script>
+<?php
+$id = $_SESSION['userid'];
+
+if(isset($id))
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=arbtop', 'root', '');
+
+    $statement = $pdo->prepare("SELECT * FROM warenkorb WHERE Benutzer_ID = '$id' ");
+
+    if ($statement->execute())
+    {
+        while ($row = $statement->fetch())
+        {
+            $pId = $row['Produkt_ID'];
+
+            $pPdo = new PDO('mysql:host=localhost;dbname=arbtop', 'root', '');
+
+            $pStatement = $pPdo->prepare("SELECT ID, Name, Preis FROM produkt WHERE ID = '$pId' ");
+
+            if ($pStatement->execute())
+            {
+                while ($pRow = $pStatement->fetch())
+                {
+                    echo '<form onsubmit="return warnung(event)" method="POST" action="warenkorb_delete_func.php">
+                            <input type="hidden" name="id" value="' . utf8_encode($pRow['ID']).'"
+                            <label>Name: ' . utf8_encode($pRow['Name']).'</label>
+                            <label>Staückpreis: ' . utf8_encode($pRow['Name']).'</label>
+                            <input style="width: 3em;" type="number" name="amountInput" min="1" max="50" value="1">
+                            <input type="submit" name="delete" value="Aus Warenkorb entfernen">
+                          </form>';
+                }
+            }
+        }
+    }
+}
+?>
+
+
 <?php
 require ('footer_war.php');
 ?>
